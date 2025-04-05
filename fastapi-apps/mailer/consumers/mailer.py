673a1093947f642fc2451c4a4bdb29e50a6cecee
pahomov aleksandr <kukikristmas@gmail.com>
2025-04-06 01:06:29 +0300

    feat: add send templates

--- fastapi-apps/mailer/consumers/mailer.py
+++ fastapi-apps/mailer/consumers/mailer.py
@@ -3,15 +3,40 @@ import json
 import aio_pika
 import aiosmtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from pathlib import Path
+from jinja2 import Template
 from core.config import settings
 
+async def load_template(template_name: str, code: str) -> str:
+    # путь к шаблонам
+    template_path = Path(__file__).parent / "static" / "templates" / template_name
+    with open(template_path, "r", encoding="utf-8") as f:
+        template = Template(f.read())
+    return template.render(code=code)
+
 async def send_email(recipient_email: str, code: str, email_type: int):
-    msg = MIMEText(code)
-    if email_type == 1: msg["Subject"] = "Подтверждение регистрации"
-    elif email_type == 2: msg["Subject"] = "Восстановление пароля"
-    elif email_type == 3: msg["Subject"] = "Данные от аккаунта"
+
+    # Определяем шаблон и тему письма
+    if email_type == 1:
+        template_name = "confirm_registration.html"
+        subject = "Подтверждение регистрации Sportium"
+    elif email_type == 2:
+        template_name = "rebuild_password.html"
+        subject = "Восстановление пароля Sportium"
+    elif email_type == 3:
+        template_name = "vk_registration.html"
+        subject = "Данные от аккаунта Sportium"
+
+    # Загружаем и рендерим шаблон
+    html_content = await load_template(template_name, code)
+
+    # Создаем MIME сообщение
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = subject
     msg["From"] = settings.smtp.email
     msg["To"] = recipient_email
+    msg.attach(MIMEText(html_content, "html"))
 
     try:
         await aiosmtplib.send(
@@ -42,12 +67,12 @@ async def consume():
 
                         email = data.get("email")
                         code = data.get("code")
-                        emai_type = int(data.get("type"))
+                        email_type = int(data.get("type"))
 
                         if not email or not code:
                             continue
 
-                        await send_email(email, code, emai_type)
+                        await send_email(email, code, email_type)
 
                     except Exception as e:
                         print(e)
